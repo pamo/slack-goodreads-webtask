e745c7be3c6a2d14d3787858b3f7415370b7f52b
Pamela Ocampo <pamela.ocampo@gmail.com>
2017-08-30 23:34:30 -0400

    Manual eslint

--- goodreads.js
+++ goodreads.js
@@ -1,8 +1,7 @@
-var request = require('request');
-var parser = require('xml2js@0.2.8').Parser({ignoreAttrs: true, mergeAttrs: true});
-var url = require('url');
-var util = require('util');
-
+const request = require('request');
+const parser = require('xml2js@0.2.8').Parser({ ignoreAttrs: true, mergeAttrs: true });
+const url = require('url');
+const util = require('util');
 
 /**
  * Takes a Book title from Slack and returns the closest match from the
@@ -18,41 +17,45 @@ module.exports = function ({ data }, callback) {
     // Passed from Slack Command
     const { title, user_name: user, response_url: responseUrl } = data;
 
-    parser.addListener('end', function (result) {
-        var response = result.GoodreadsResponse.search[0].results[0].work[0].best_book[0];
-        var found_title = response.title[0];
-        var title_url = url.parse(found_title).href;
+    parser.addListener('end', (result) => {
+        const response = result.GoodreadsResponse.search[0].results[0].work[0].best_book[0];
+        const foundTitle = response.title[0];
+        const titleUrl = url.parse(foundTitle).href;
 
         if (typeof response !== 'undefined') {
-            request.post({url: responseUrl, method: 'POST', json: true, body: {
-                'response_type': 'in_channel',
-                'text': util.format('*Goodreads*: %s attempted to reference *%s* from Goodreads. This was the closest match we could find.', user, title),
-                'attachments': [
-                    {
-                        'title': found_title,
-                        'title_link': 'https://www.goodreads.com/book/title?id=' + title_url,
-                        'thumb_url': response.small_image_url[0]
-                    }
-                ]
-            }});
+            request.post({
+                url: responseUrl, method: 'POST', json: true, 
+                body: {
+                    response_type: 'in_channel',
+                    text: util.format('*Goodreads*: %s attempted to reference *%s* from Goodreads. This was the closest match we could find.', user, title),
+                    attachments: [{
+                        title: foundTitle,
+                        title_link: `https://www.goodreads.com/book/title?id=${titleUrl}`,
+                        thumb_url: response.small_image_url[0]
+                    }]
+                }
+            });
         } else {
-            request.post({url: responseUrl, method: 'POST', json: true, body: {
-                'response_type': 'ephemeral',
-                'text': 'The book you were looking for cannot be found.'
-            }});
+            request.post({
+                url: responseUrl, method: 'POST', json: true,
+                body: {
+                    response_type: 'ephemeral',
+                    text: 'The book you were looking for cannot be found.'
+                }
+            });
         }
     });
 
-    var req = request.get(`https://www.goodreads.com/search/index.xml?key=V0xMCsWPQx5V8S1TXoEw&q=${title}`, function (res) {
-        res.on('data', function (data) {
+    const req = request.get(`https://www.goodreads.com/search/index.xml?key=V0xMCsWPQx5V8S1TXoEw&q=${title}`, (res) => {
+        res.on('data', (data) => {
             parser.parseString(data);
         });
 
         return(callback(null, 'Fetching Data...'));
     });
+    
     req.end();
-
-    req.on('error', function (e) {
+    req.on('error', (e) => {
         return(callback(e));
     });
 };
